5b7fb033337e743c8430b590920ccd206d647168
holmesie <aholmes@mitre.org>
2016-11-25 14:39:22 -0500

    Added Gitlab CI config; Added Rubocop; Added admin rake tasks

--- test/controllers/certificates_controller_test.rb
+++ test/controllers/certificates_controller_test.rb
@@ -1,9 +1,8 @@
 require 'test_helper'
 
 class CertificatesControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
+  test 'should redirect if not signed in' do
     get certificates_index_url
-    assert_response :success
+    assert_response :redirect
   end
-
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,22 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    assert_equal 0, User.count
+    @user = User.create!(email: 'test@test.com', password: 'Password123!')
+  end
+
+  test 'confirmed' do
+    assert_equal false, @user.confirmed?
+    @user.confirm
+    assert_equal true, @user.confirmed?
+  end
+
+  test 'grant and revoke admin' do
+    assert_equal false, @user.admin?
+    @user.grant_admin
+    assert_equal true, @user.admin?
+    @user.revoke_admin
+    assert_equal false, @user.admin?
+  end
 end
