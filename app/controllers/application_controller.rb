7612aa169355af91bd01b76e3b8169366efff507
holmesie <aholmes@mitre.org>
2017-02-27 17:28:00 -0500

    More UI Changes
    - Recreated Sign In/Sign Up/etc. pages
    - Adjusted how extra account properties are handled
    - Changed how devise visually handles errors
    - Made devise timeout after 30 minutes of inactivity
    - Lots of CSS refactoring

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,7 @@ class ApplicationController < ActionController::Base
   include Pundit
   before_action :authenticate_user!
   protect_from_forgery with: :exception
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
 
@@ -11,5 +12,12 @@ class ApplicationController < ActionController::Base
   def user_not_authorized
     flash[:danger] = "You are not authorized to perform this action."
     redirect_to(request.referrer || root_path)
+  end
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :telephone, :email])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :telephone, :email])
   end
 end
