fc2f305b9601a1ce2db6a3c376be5c95b996b46d
holmesie <aholmes@mitre.org>
2017-10-31 11:05:45 -0400

    Fixed issue with age of records pie chart; Added city model test

---
+++ test/models/city_test.rb
@@ -0,0 +1,41 @@
+require 'test_helper'
+
+class CityTest < ActiveSupport::TestCase
+  test 'valid cities exist' do
+    assert true, City.where(name: 'Amityville').count > 0
+    assert true, City.where(name: 'Deer Grove').count > 0
+    assert true, City.where(name: 'Aguila').count > 0
+    assert true, City.where(name: 'Youngtown').count > 0
+    assert true, City.where(name: 'Pepeekeo').count > 0
+  end
+
+  test 'cities in correct county' do
+    correct = false
+    City.where(name: 'Billerica').each do |city|
+      correct = city.county.name == 'MIDDLESEX'
+      break if correct
+    end
+    assert correct
+    correct = false
+    City.where(name: 'Boston').each do |city|
+      correct = city.county.name == 'SUFFOLK'
+      break if correct
+    end
+    assert correct
+  end
+
+  test 'cities in correct state' do
+    correct = false
+    City.where(name: 'Manchester').each do |city|
+      correct = city.state.name == 'New Hampshire'
+      break if correct
+    end
+    assert correct
+    correct = false
+    City.where(name: 'Atlanta').each do |city|
+      correct = city.state.name == 'Georgia'
+      break if correct
+    end
+    assert correct
+  end
+end
