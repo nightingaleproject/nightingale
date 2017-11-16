43e9163de2f3b6c65aee76f38d3a95c93c44bf2e
Pete K <krautscheid@mitre.org>
2017-11-16 15:26:26 -0500

    Set reasonable base controller for API

--- app/controllers/api/v1/death_records_controller.rb
+++ app/controllers/api/v1/death_records_controller.rb
@@ -1,4 +1,4 @@
-class Api::V1::DeathRecordsController < Api::V1::ApiController
+class Api::V1::DeathRecordsController < ApplicationController
   before_action :doorkeeper_authorize!
 
   # POST death_record
