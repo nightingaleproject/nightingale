8ff4b28e9584308fc04ee710c7a4d608304eeaca
holmesie <aholmes@mitre.org>
2017-11-27 12:04:32 -0500

    Adjusted results check for audit table spec

--- spec/support/features/pages/audit_log.rb
+++ spec/support/features/pages/audit_log.rb
@@ -1,14 +1,9 @@
 module Pages
-    class AuditLog
-      include Capybara::DSL
+  class AuditLog
+    include Capybara::DSL
   
-      def search(search_string)
-        find(:xpath, '//*[@id="audits_table_filter"]/label/input').set(search_string)
-      end
-      
-      def record_count
-        return all(:xpath, '//*[@id="audits_table"]/tbody/tr').count
-      end
+    def search(search_string)
+      find(:xpath, '//*[@id="audits_table_filter"]/label/input').set(search_string)
     end
   end
-  
+end
