d8f978a4f64e89965faef177f4a6ed0813386960
Adam M. Holmes <aholmes@mitre.org>
2018-12-10 14:19:23 -0500

    Made api/v1 for POSTing internal records more like FHIR api

--- app/controllers/api/v1/death_records_controller.rb
+++ app/controllers/api/v1/death_records_controller.rb
@@ -1,52 +1,62 @@
-class Api::V1::DeathRecordsController < ActionController::Base
-  protect_from_forgery prepend: true, with: :exception
+class Api::V1::DeathRecordsController < ActionController::API
+  include ActionController::MimeResponds
   before_action :doorkeeper_authorize!
+  before_action :cors_headers
 
   # POST death_record
   def create
     respond_to do |format|
-      # create death record
       physician_email = params[:physician_email]
 
       if !physician_email
-        msg = { status: 'failure', message: 'Bad physician email' }
+        user = User.find_by(first_name: 'Example', last_name: 'Certifier')
       else
-        physician = User.find_for_authentication(email: physician_email)
-        workflow = Workflow.where(initiator_role: physician.roles.first.name).order('created_at').last
-        step_flow = workflow.step_flows.first
-        @death_record = DeathRecord.new(creator: physician,
-                                        owner: physician,
-                                        workflow: workflow,
-                                        step_flow: step_flow)
-
-        # Create and and set a StepStatus for this DeathRecord.
-        step_status = StepStatus.create(death_record: @death_record,
-                                        current_step: step_flow.current_step,
-                                        next_step: step_flow.next_step,
-                                        previous_step: step_flow.previous_step)
-        @death_record.step_status = step_status
-        @death_record.save
-
-        steps_content_hash = @death_record.separate_step_contents(params[:contents])
-        @death_record.steps.each do |step|
-          if steps_content_hash[step.name]
-            StepContent.update_or_create_new(death_record: @death_record,
-                                             step: step,
-                                             contents: steps_content_hash[step.name],
-                                             editor: physician)
-          end
-        end
-
-        Rails.logger.debug 'Saving new decedent'
-        # save but do not validate for now, as validation will not pass since
-        # not all data will be in place
-        # TBD:  Consider validation for this scenario
-        @death_record.save(validate: false)
-        msg = { status: 'ok', message: "Successfully added new death record with ID: #{@death_record.id}" }
+        user = User.find_for_authentication(email: physician_email)
       end
 
-      format.json { render json: msg }
+      contents = JSON.parse(request.body.string)
+
+      workflow = Workflow.where(initiator_role: user.roles.first.name).order('created_at').last
+      step_flow = workflow.step_flows.first
+      @death_record = DeathRecord.new(creator: user,
+                                      owner: user,
+                                      workflow: workflow,
+                                      step_flow: step_flow)
+      step_status = StepStatus.create(death_record: @death_record,
+                                      current_step: step_flow.current_step,
+                                      next_step: step_flow.next_step,
+                                      previous_step: step_flow.previous_step)
+      @death_record.step_status = step_status
+      @death_record.save
+      steps_content_hash = @death_record.separate_step_contents(contents)
+      @death_record.steps.each do |step|
+        if steps_content_hash[step.name]
+          StepContent.update_or_create_new(death_record: @death_record,
+                                           step: step,
+                                           contents: steps_content_hash[step.name],
+                                           editor: user)
+        end
+      end
+      @death_record.notify = true
+      @death_record.save(validate: false)
+
+      format.json { render json: { status: :ok, message: 'Created ID: ' + @death_record.id.to_s } }
+      format.xml { render xml: { status: :ok, message: 'Created ID: ' + @death_record.id.to_s } }
     end
+  end
+
+  # Handle OPTIONS requests for CORS preflight
+  def options
+    render plain: ''
+  end
+
+  private
+
+  # Allow cross-origin requests
+  def cors_headers
+    headers['Access-Control-Allow-Origin'] = '*'
+    headers['Access-Control-Allow-Headers'] = '*'
+    headers['Access-Control-Allow-Methods'] = 'GET, POST, PUT, DELETE, OPTIONS'
   end
 
 end
